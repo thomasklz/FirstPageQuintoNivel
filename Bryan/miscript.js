6cc6dc9b4f7c8613bdd00cd3780d684872caa775
thomasklz <pipos_rgt@hotmail.com>
2020-07-05 18:49:25 -0500

    Bryan

---
+++ Bryan/miscript.js
@@ -0,0 +1,59 @@
+function validacion(){
+ debugger;
+var usuario=document.getElementById("usuario").value;
+var contrase=document.getElementById("contra").value;
+
+if (usuario=="bryan" && contrase=="123"  ) {
+	
+ 	 window.location="sevicios.html";
+ 	 
+ }	
+else{
+	debugger;
+	alert("LOS DATOS NO SON CORRECTOS") ;
+	
+ }	
+
+   
+
+}
+function ir_servicios(){
+ debugger;
+
+
+	
+ 	 window.location="tipos_servicios.html";
+ 	
+   
+
+}
+function gastroenterologo(){
+ debugger;
+
+
+	
+ 	 window.location="gastroenterologo.html";
+ 	
+   
+
+}
+function pediatra(){
+ debugger;
+
+
+	
+ 	 window.location="pediatra.html";
+ 	
+   
+
+}
+function endocrinologo(){
+ debugger;
+
+
+	
+ 	 window.location="endocrinologo.html";
+ 	
+   
+
+}
